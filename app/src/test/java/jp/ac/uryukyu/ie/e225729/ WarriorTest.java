ca5d8a02e9ff6bf1a456416ea0b40a7aacbb51ef
梅田敬祐 <umedatakashiyuu@umedatakashiyuunoMacBook-Air.local>
2022-12-01 17:55:53 +0900

    ヤ

--- app/src/test/java/jp/ac/uryukyu/ie/e225729/ WarriorTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e225729/ WarriorTest.java
@@ -7,7 +7,7 @@ class WarriorTest {
             int enemyHp = 1000;
             int attack = 100;
             Warrior demoWarrior = new Warrior("デモ戦士", 1, attack);
-            Enemy slime = new Enemy("スライムもどき", enemyHp, 1);
+            Enemy slime = new Enemy("スライムみたいなやつ", enemyHp, 1);
             for(int count = 0; count < 3; count++){
                 enemyHp = slime.hitPoint;
                 demoWarrior.attackWithWeponSkill(slime);
